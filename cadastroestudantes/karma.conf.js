b849b3d8e33c251db4c9aa0bde3960c8c4f1e37e
Grazielle127 <grazigrazielle454@gmail.com>
2022-10-13 12:03:45 -0300

    alteracao da versao no projeto

--- cadastroestudantes/karma.conf.js
+++ cadastroestudantes/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/cadastroestudantes'),
+      dir: require('path').join(__dirname, './coverage/cadastro-estudantes'),
       subdir: '.',
       reporters: [
         { type: 'html' },
